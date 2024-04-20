2fe9c136257943cc30d7e81695e21723f9b69ca0
gabilbck <gabrielieduardalembeck@gmail.com>
2024-04-19 21:29:59 -0300

    aadd

---
+++ aula01/script.js
@@ -0,0 +1,13 @@
+function imprimirPagina(){
+    document.write('Ferramentas WEB');
+}
+
+function tamanhoFonte(){
+    document.getElementById('texto').style.fontSize='30px';
+}
+
+function adicionaConteudo(){
+    document.getElementById('inserir').inerHTML = Date();
+}
+
+f
